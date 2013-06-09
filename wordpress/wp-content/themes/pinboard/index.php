2d73089fd82f4cdd0c9bdcfdfb3ae6d4dd4d9d55
eru <eru.penkman@gmail.com>
2013-06-09 22:45:11 +1000

    now support any menu name

--- wordpress/wp-content/themes/pinboard/index.php
+++ wordpress/wp-content/themes/pinboard/index.php
@@ -8,11 +8,12 @@
 	<?php elseif( ( is_home() && is_paged() ) || ( ! is_home() && pinboard_get_option( 'location' ) ) ) : ?>
 		<?php pinboard_current_location(); ?>
 	<?php endif; ?>
+	
 	<div id="container">
 		<section id="content" <?php  pinboard_content_class(); ?>>
 			<?php if( have_posts() ) : ?>
+			
 				<div class="entries">
-					
 					<?php 
 						global $wp_query;
 						pinboard_category_filter( pinboard_get_option( 'portfolio_cat' ) ); 
@@ -34,6 +35,7 @@
 						<?php get_template_part( 'content', get_post_format() ); ?>
 					<?php endwhile; ?>
 				</div><!-- .entries -->
+				
 				<?php pinboard_posts_nav(); ?>
 			<?php else : ?>
 				<?php pinboard_404(); ?>
@@ -44,4 +46,5 @@
 		<?php endif; ?>
 		<div class="clear"></div>
 	</div><!-- #container -->
+	
 <?php get_footer(); ?>
